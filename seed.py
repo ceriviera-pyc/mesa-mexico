efef5977f34b8fc534b0470bf109ed085717d09e
ceriviera-pyc <ceriviera@gmail.com>
2026-09-10 17:59:22 -0500

    SaaS Update: Sincronizacion automatizada de codigo limpio

--- seed.py
+++ seed.py
@@ -2,7 +2,7 @@ import models
 from database import SessionLocal, engine
 
 # Aseguramos que las tablas se borren y se vuelvan a crear con las nuevas columnas
-models.Base.metadata.drop_all(bind=engine)   # 👈 AGREGA ESTA LÍNEA AQUÍ
+models.Base.metadata.drop_all(bind=engine)   
 models.Base.metadata.create_all(bind=engine)
 
 

13ab9b9e2be6169617cfe594909050a6b6f09d55
ceriviera-pyc <ceriviera@gmail.com>
2026-09-11 15:00:27 -0500

    fix: remover drop_all para evitar bloqueos en el despliegue de la nube

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ app.add_middleware(
 
 
 # 🚀 Esto obliga a Render a tirar la base de datos vieja y crear las nuevas columnas de GPS, teléfono y PIN
-models.Base.metadata.drop_all(bind=database.engine)
 models.Base.metadata.create_all(bind=database.engine)
 
 # 📁 Montar la carpeta static para los archivos CSS y JS
